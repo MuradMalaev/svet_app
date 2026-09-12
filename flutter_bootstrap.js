c052344c87fa2f2576537dd50d294511d5530966
MuradMalaev <m7mtigr2@gmail.com>
2026-09-12 12:42:53 +0300

    Fix surah accent and search, restore isnad/sharh, remove azkars/99 names, move settings to Quran screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1030640111" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3226452315" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
